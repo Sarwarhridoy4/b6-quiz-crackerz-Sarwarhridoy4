3c19b6c5fea94b7929bfda37b7ab9ad6a068b581
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-10 22:27:28 +0600

    routes are seted successfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Topic from './components/Topic/Topic';
+import Main from './Layouts/Main';
 
 function App() {
   //creating router
   const router = createBrowserRouter([
     {
       path: '/',
-      element:
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element:<Home></Home>
+        },
+        {
+          path: '/topic',
+          element:<Topic></Topic>
+        },
+        {
+          path: '/statistics',
+          element:<Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element:<Blogs></Blogs>
+        }
+      ]
     }
   ])
   return (
--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Outlet></Outlet>
         </div>
     );
 };
