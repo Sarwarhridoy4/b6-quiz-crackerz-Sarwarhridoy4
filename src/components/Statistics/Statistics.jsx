a2b39b52e645bc8efeba9760ed33d1c7927c49c1
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-11 12:23:34 +0600

    data loaded for statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h2>This is Statistics Page</h2>
-        </div>
-    );
+    const information = useLoaderData().data;
+    console.log(information);
+  return (
+    <div>
+      <h2>This is Statistics Page</h2>
+    </div>
+  );
 };
 
 export default Statistics;
