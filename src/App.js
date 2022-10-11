32499ac195347c52246e92e5b120f80de609013f
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-11 16:11:52 +0600

    Statics page complited

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ function App() {
         },
         {
           path: '/statistics',
-          loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz'),
           element:<Statistics></Statistics>
         },
         {
