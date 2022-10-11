cb7bf54dad2c6f0768070cef56246436fbb7cc83
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-11 08:34:52 +0600

    header edited

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,17 +5,14 @@ const Banner = () => {
     <div>
       <div className='hero min-h-screen bg-base-200'>
         <div className='hero-content flex-col lg:flex-row'>
-          <img src='https://placeimg.com/260/400/arch' alt=""
-            className='max-w-sm rounded-lg shadow-2xl'
+          <img src='https://cdn.pixabay.com/photo/2018/03/21/05/29/question-mark-3245622_960_720.jpg' alt=""
+            className='w-full rounded-lg shadow-2xl'
           />
           <div>
-            <h1 className='text-5xl font-bold'>Box Office News!</h1>
+            <h1 className='text-5xl font-bold'>Quizzily Daily Quiz</h1>
             <p className='py-6'>
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Quizzily has arrage a quiz for the developer to polish up their knowledge on daily basis by accomplising some quiz on desire topics. 
             </p>
-            <button className='btn btn-primary'>Get Started</button>
           </div>
         </div>
       </div>
