c986a94466b223a86345e4224a0a9f21904cd4ba
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-11 17:07:52 +0600

    back to home added with 404 page

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div className='bg-amber-400 w-full h-[100vh] flex flex-col items-center justify-center'>
       <img className="w-1/4" src="https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562_960_720.png" alt='Page Not Found' />
-      <h1 className='text-6xl font-bold'>Sorry! Page Not Found!</h1>
+          <h1 className='text-6xl font-bold'>Sorry! Page Not Found!</h1>
+          <Link to='/'>
+          <button className="btn btn-error m-10">Back Home</button>
+          </Link>
     </div>
   );
 };
